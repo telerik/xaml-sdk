06ecb10ffa7342736d94da0720e40b028a833c73
Vladimir Amiorkov <v.amiorkov.blogs@gmail.com>
2013-08-16 17:23:15 +0300

    Updated RestrictedToParent example

--- Window/RestrictedToParent/MainWindow.xaml.cs
+++ Window/RestrictedToParent/MainWindow.xaml.cs
@@ -31,21 +31,89 @@ namespace RestrictedToParent
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            this.MyRadWindow.Show();
+            if (!this.MyRadWindow.IsVisible)
+            {
+                this.MyRadWindow.Show();
+            }
         }
 
         private void Window_SizeChanged_1(object sender, SizeChangedEventArgs e)
         {
             var primaryScreenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
             var primaryScreenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            if (this.HasMultipleMonitors())
+            {
+                primaryScreenWidth = System.Windows.SystemParameters.PrimaryScreenWidth * 2;
+            }
+
             var window = sender as Window;
-            double rightMargin = primaryScreenWidth - (window.Left + window.Width);
-            double bottomMargin = primaryScreenHeight - (window.Top + window.Height);
+            double rightMargin;
+            double bottomMargin;
+            double topMargin;
+            double leftMargin;
+            if (window.WindowState == System.Windows.WindowState.Maximized)
+            {
+                if (this.HasMultipleMonitors())
+                {
+                    var monitorNumber = this.GetMonitorNumber(window.Left);
+                    if (monitorNumber > 1)
+                    {
+                        rightMargin = 0;
+                        leftMargin = System.Windows.SystemParameters.PrimaryScreenWidth;
+                    }
+                    else
+                    {
+                        rightMargin = System.Windows.SystemParameters.PrimaryScreenWidth;
+                        leftMargin = 0;
+                    }
+                    bottomMargin = 0;
+                    topMargin = 0;
+                }
+                else
+                {
+                    rightMargin = primaryScreenWidth - System.Windows.SystemParameters.PrimaryScreenWidth;
+                    bottomMargin = primaryScreenHeight - System.Windows.SystemParameters.PrimaryScreenHeight;
+                    topMargin = 0;
+                    leftMargin = 0;
+                }
+            }
+            else
+            {
+                rightMargin = primaryScreenWidth - (window.Left + window.Width);
+                bottomMargin = primaryScreenHeight - (window.Top + window.Height);
+                topMargin = window.Top;
+                leftMargin = window.Left;
+            }
 
             if (window != null)
             {
-                this.MyRadWindow.RestrictedAreaMargin = new Thickness(window.Left, window.Top, rightMargin, bottomMargin);
+                this.MyRadWindow.RestrictedAreaMargin = new Thickness(leftMargin, topMargin, rightMargin, bottomMargin);
             }
+        }
+
+        private int GetMonitorNumber(double monitorLeftCoordinate)
+        {
+            var primaryScreenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            var result = 1;
+            while (primaryScreenWidth < monitorLeftCoordinate)
+            {
+                result++;
+                primaryScreenWidth = primaryScreenWidth * 2;
+            }
+
+            return result;
+        }
+
+        private bool HasMultipleMonitors()
+        {
+            Screen[] screensArray = Screen.AllScreens;
+            List<Screen> screensList = screensArray.OfType<Screen>().ToList();
+            if (screensList.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         private void Window_LocationChanged_1(object sender, EventArgs e)
