bbb67e1f93d77a8092a87ddc9ccf44ff1c1f4e92
VladimirAmiorkov <v.amiorkov@gmail.com>
2014-03-20 15:22:55 +0200

    Updated DragDrop examples

---
+++ DragDrop/DragDropUsingDragDropPayloadManager/Customer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DragDropUsingDragDropPayloadManager
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
