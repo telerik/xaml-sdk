bd08abef8a7db7a03d86a82d41270049e9b1eee3
Pavel Pavlov <pavel.raykov.pavlov@gmail.com>
2014-11-10 17:04:59 +0200

    Update the code of the demo

--- ChartView/SL/ChartDataSource/SimpleViewModel.cs
+++ ChartView/SL/ChartDataSource/SimpleViewModel.cs
@@ -5,38 +5,20 @@ using Telerik.Windows.Data;
 
 namespace ChartDataSource
 {
-	public class SimpleViewModel : DependencyObject
+	public class SimpleViewModel
 	{
-		/// <summary>
-		/// Identifies the <see cref="Data"/> dependency property.
-		/// </summary>
-		public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data",
-			typeof(ObservableCollection<SalesInfo>),
-			typeof(SimpleViewModel),
-			new PropertyMetadata(null));
-
-		public RadObservableCollection<SalesInfo> Data
-		{
-			get
-			{
-				return (RadObservableCollection<SalesInfo>)this.GetValue(DataProperty);
-			}
-			set
-			{
-				this.SetValue(DataProperty, value);
-			}
-		}
-
+		public RadObservableCollection<SalesInfo> Data { get; set; }
+		
 		public SimpleViewModel()
 		{
-			var data = new RadObservableCollection<SalesInfo>();
-			DateTime startDate = new DateTime(2012, 12, 15);
-			for (int i = 0; i < 20; i += 1)
-			{
-				data.Add(new SalesInfo() { Time = startDate.AddDays(i), Value = i });
-			}
-
-			this.Data = data;
+		var data = new RadObservableCollection<SalesInfo>();
+		DateTime startDate = new DateTime(2012, 12, 15);
+		for (int i = 0; i < 20; i += 1)
+		{
+			data.Add(new SalesInfo() { Time = startDate.AddDays(i), Value = i });
+		}
+		
+		this.Data = data;
 		}
 	}
 }
