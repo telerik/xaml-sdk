75b595f06858a76f22be2fbdbcddd4dd4189ce21
pavelrpavlov <pavel.raykov.pavlov@gmail.com>
2015-01-29 17:50:23 +0200

    Weekly Update

---
+++ RichTextBox/CustomLayerAndCustomAnnotation/CustomUILayersBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Telerik.Windows.Documents.UI.Layers;
+
+namespace CustomLayerAndCustomAnnotation
+{
+    public class CustomUILayersBuilder : UILayersBuilder
+    {
+        protected override void BuildUILayersOverride(Telerik.Windows.Documents.UI.IUILayerContainer uiLayerContainer)
+        {
+            base.BuildUILayersOverride(uiLayerContainer);
+
+            uiLayerContainer.UILayers.AddLast(new CustomRangeLayer());
+        }
+    }
+}
