dd1ee69b0350ccccea2e14c1c57041fff631d437
PetyaSotirova <Petya.Sotirova@telerik.com>
2015-02-12 12:45:14 +0200

    Add MailMerge example for WordsProcessing

---
+++ WordsProcessing/MailMerge/DynamicObjectGetMemberBinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace MailMerge
+{
+    public class DynamicObjectGetMemberBinder : GetMemberBinder
+    {
+        public DynamicObjectGetMemberBinder(string name, bool ignoreCase)
+            : base(name, ignoreCase)
+        {
+        }
+
+        public override DynamicMetaObject FallbackGetMember(DynamicMetaObject target, DynamicMetaObject errorSuggestion)
+        {
+            return target;
+        }
+    }
+}
---
+++ WordsProcessing/MailMerge/DynamicObjectSetMemberBinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace MailMerge
+{
+    public class DynamicObjectSetMemberBinder : SetMemberBinder
+    {
+        public DynamicObjectSetMemberBinder(string name, bool ignoreCase)
+            : base(name, ignoreCase)
+        {
+        }
+
+        public override DynamicMetaObject FallbackSetMember(DynamicMetaObject target, DynamicMetaObject value, DynamicMetaObject errorSuggestion)
+        {
+            return target;
+        }
+    }
+}
