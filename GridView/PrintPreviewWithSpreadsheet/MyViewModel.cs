133d63ca53cc8500a24321f251441de6454b4d40
martinivanoff <martin.ivanov@telerik.com>
2015-12-11 11:15:55 +0200

    Updated examples

---
+++ GridView/PrintPreviewWithSpreadsheet/MyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace PrintPreviewWithSpreadsheet
+{
+    public class MyViewModel
+    {
+      
+        private ObservableCollection<Club> clubs;
+        public ObservableCollection<Club> Clubs
+        {
+            get
+            {
+                if (this.clubs == null)
+                {
+                    this.clubs = Club.GetClubs();
+                }
+
+                return this.clubs;
+            }
+        }
+    }
+}
---
+++ GridView/PrintPreviewWithSpreadsheet/Readme.txt
@@ -0,0 +1,3 @@
+
+# Print Preview with RadSpreadSheet #
+This example demonstrates how to implement PrintPreview functionality for RadGridView using the RadSpreadsheet control.
