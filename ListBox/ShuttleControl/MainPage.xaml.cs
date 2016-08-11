8f7de4e68e33c1a3b3a4594fe319d764448f85c6
martinivanoff <martin.ivanov@telerik.com>
2016-08-11 16:28:13 +0300

    Examples update

---
+++ ListBox/ShuttleControl/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ShuttleControl
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
