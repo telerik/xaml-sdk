8f38eae1ae9ad2408564d41ea15d0301a946f551
GitTask <gittask@notused.com>
2023-01-18 20:24:02 +0200

    New version of Telerik SDK examples.

--- TreeView/EnableOnlyDropInsideItem/MainPage.xaml.cs
+++ TreeView/EnableOnlyDropInsideItem/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EnableOnlyDropInsideItem.ViewModels;
+using System;
 using System.Linq;
+using System.Collections;
 using System.Windows.Controls;
 using Telerik.Windows.Controls.TreeView;
 using Telerik.Windows.DragDrop;
@@ -17,11 +19,34 @@ namespace EnableOnlyDropInsideItem_SL
         private void OnDragOver(object sender, Telerik.Windows.DragDrop.DragEventArgs e)
         {
             var options = DragDropPayloadManager.GetDataFromObject(e.Data, TreeViewDragDropOptions.Key) as TreeViewDragDropOptions;
-            if (options.DropPosition != Telerik.Windows.Controls.DropPosition.Inside)
+            var draggedItem = options.DraggedItems.ElementAt(0);
+            var dropItem = options.DropTargetItem.Item;
+            var dropItemChildrenType = GetChildCollectionType(dropItem);
+            var dragItemType = draggedItem.GetType();
+            var dropItemType = dropItem.GetType();
+
+            if ((dragItemType == dropItemType &&
+                dropItemChildrenType != dragItemType &&
+                options.DropPosition == Telerik.Windows.Controls.DropPosition.Inside) ||
+                (dragItemType != dropItemChildrenType && dragItemType != dropItemType))
             {
-                options.DropPosition = Telerik.Windows.Controls.DropPosition.Inside;
+                options.DropAction = DropAction.None;
                 options.UpdateDragVisual();
             }
+        }
+
+        private static Type GetChildCollectionType(object dataItem)
+        {
+            if (dataItem is League)
+            {
+                return typeof(Division);
+            }
+            else if (dataItem is Division)
+            {
+                return typeof(Team);
+            }
+
+            return null;
         }
     }
 }
--- TreeView/EnableOnlyDropInsideItem/MainWindow.xaml.cs
+++ TreeView/EnableOnlyDropInsideItem/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EnableOnlyDropInsideItem.ViewModels;
+using System;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Telerik.Windows.Controls.TreeView;
 using Telerik.Windows.DragDrop;
 
@@ -31,11 +22,34 @@ namespace EnableOnlyDropInsideItem
         private void OnDragOver(object sender, Telerik.Windows.DragDrop.DragEventArgs e)
         {
             var options = DragDropPayloadManager.GetDataFromObject(e.Data, TreeViewDragDropOptions.Key) as TreeViewDragDropOptions;
-            if (options.DropPosition != Telerik.Windows.Controls.DropPosition.Inside)
+            var draggedItem = options.DraggedItems.ElementAt(0);
+            var dropItem = options.DropTargetItem.Item;
+            var dropItemChildrenType = GetChildCollectionType(dropItem);
+            var dragItemType = draggedItem.GetType();
+            var dropItemType = dropItem.GetType();
+
+            if ((dragItemType == dropItemType &&
+                dropItemChildrenType != dragItemType &&
+                options.DropPosition == Telerik.Windows.Controls.DropPosition.Inside) ||
+                (dragItemType != dropItemChildrenType && dragItemType != dropItemType))
             {
-                options.DropPosition = Telerik.Windows.Controls.DropPosition.Inside;
+                options.DropAction = DropAction.None;
                 options.UpdateDragVisual();
             }
+        }
+
+        private static Type GetChildCollectionType(object dataItem)
+        {
+            if (dataItem is League)
+            {
+                return typeof(Division);
+            }
+            else if (dataItem is Division)
+            {
+                return typeof(Team);
+            }
+
+            return null;
         }
     }
 }
